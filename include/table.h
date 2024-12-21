6ddb16f05f06255ba26d3e51bbb25b31c459200e
leezhi <leezhi@gmail.com>
2024-12-21 14:46:31 +0800

    leezhi

--- include/table.h
+++ include/table.h
@@ -5,8 +5,10 @@
 #include "obstacle.h"
 
 class Table : public Obstacle {
+private:
+    const char* texturePath;  // 增加材質路徑成員
 public:
-    Table(SDL_Renderer* renderer, int x, int y, int w, int h);
+    Table(SDL_Renderer* renderer, int x, int y, int w, int h, const char* texturePath);
     void render() override;
     // Table specific functionality
     bool canPlaceItem() const;
