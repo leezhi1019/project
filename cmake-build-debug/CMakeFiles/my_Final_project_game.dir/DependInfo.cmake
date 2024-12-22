621b1cc0ae5e1b573870f7cb6db984ba32f4b217
beyond1125 <danielsu1125@gmail.com>
2024-12-22 14:54:21 +0800

    add gitignore to main

--- cmake-build-debug/CMakeFiles/my_Final_project_game.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/my_Final_project_game.dir/DependInfo.cmake
@@ -8,16 +8,16 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "C:/Users/leezhi/FINAL/project/main.cpp" "CMakeFiles/my_Final_project_game.dir/main.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/main.cpp.obj.d"
-  "C:/Users/leezhi/FINAL/project/src/bookshelf.cpp" "CMakeFiles/my_Final_project_game.dir/src/bookshelf.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/bookshelf.cpp.obj.d"
-  "C:/Users/leezhi/FINAL/project/src/character.cpp" "CMakeFiles/my_Final_project_game.dir/src/character.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/character.cpp.obj.d"
-  "C:/Users/leezhi/FINAL/project/src/engine.cpp" "CMakeFiles/my_Final_project_game.dir/src/engine.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/engine.cpp.obj.d"
-  "C:/Users/leezhi/FINAL/project/src/menu.cpp" "CMakeFiles/my_Final_project_game.dir/src/menu.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/menu.cpp.obj.d"
-  "C:/Users/leezhi/FINAL/project/src/obstacle.cpp" "CMakeFiles/my_Final_project_game.dir/src/obstacle.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/obstacle.cpp.obj.d"
-  "C:/Users/leezhi/FINAL/project/src/playground.cpp" "CMakeFiles/my_Final_project_game.dir/src/playground.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/playground.cpp.obj.d"
-  "C:/Users/leezhi/FINAL/project/src/settings.cpp" "CMakeFiles/my_Final_project_game.dir/src/settings.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/settings.cpp.obj.d"
-  "C:/Users/leezhi/FINAL/project/src/table.cpp" "CMakeFiles/my_Final_project_game.dir/src/table.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/table.cpp.obj.d"
-  "C:/Users/leezhi/FINAL/project/src/tool.cpp" "CMakeFiles/my_Final_project_game.dir/src/tool.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/tool.cpp.obj.d"
+  "C:/finalproject/project/main.cpp" "CMakeFiles/my_Final_project_game.dir/main.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/main.cpp.obj.d"
+  "C:/finalproject/project/src/bookshelf.cpp" "CMakeFiles/my_Final_project_game.dir/src/bookshelf.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/bookshelf.cpp.obj.d"
+  "C:/finalproject/project/src/character.cpp" "CMakeFiles/my_Final_project_game.dir/src/character.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/character.cpp.obj.d"
+  "C:/finalproject/project/src/engine.cpp" "CMakeFiles/my_Final_project_game.dir/src/engine.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/engine.cpp.obj.d"
+  "C:/finalproject/project/src/menu.cpp" "CMakeFiles/my_Final_project_game.dir/src/menu.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/menu.cpp.obj.d"
+  "C:/finalproject/project/src/obstacle.cpp" "CMakeFiles/my_Final_project_game.dir/src/obstacle.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/obstacle.cpp.obj.d"
+  "C:/finalproject/project/src/playground.cpp" "CMakeFiles/my_Final_project_game.dir/src/playground.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/playground.cpp.obj.d"
+  "C:/finalproject/project/src/settings.cpp" "CMakeFiles/my_Final_project_game.dir/src/settings.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/settings.cpp.obj.d"
+  "C:/finalproject/project/src/table.cpp" "CMakeFiles/my_Final_project_game.dir/src/table.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/table.cpp.obj.d"
+  "C:/finalproject/project/src/tool.cpp" "CMakeFiles/my_Final_project_game.dir/src/tool.cpp.obj" "gcc" "CMakeFiles/my_Final_project_game.dir/src/tool.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
